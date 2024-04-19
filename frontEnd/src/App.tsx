a2fb01e299f682a4af05b17e3f3adaf0dc404547
zidbon <zidbon@hotmail.com>
2024-04-19 01:50:23 -0700

    Beginning of a Navbar

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,8 +1,13 @@
+import NavBar from "./components/NavBar";
+
 function App() {
   return (
-    <main className="py-10">
-      <h1>BSPWM for the masses!</h1>
-    </main>
+    <>
+      <NavBar />
+      <main className="py-10">
+        <h1>BSPWM for the masses!</h1>
+      </main>
+    </>
   );
 }
 
---
+++ frontEnd/src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+const NavBar = () => {
+  return (
+    <>
+      <div className="mx-auto py-2 my-2 px-2 sm:px-6 lg:px-8 bg-black">
+        <div className="text-3xl text-white">BSPWM</div>
+        <div className="text-sm text-white">For the masses!</div>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
