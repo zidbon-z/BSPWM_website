1c297fa3fdeeeb7baaf15f88a934c56d00dc5417
zidbon <zidbon@hotmail.com>
2024-04-20 11:42:46 -0700

    Created a logo and put it on the navbar

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <NavBar />
-      <main className="py-10">
+      <main className="py-10 bg-zinc-900 text-white">
         <h1>BSPWM for the masses!</h1>
       </main>
     </>
